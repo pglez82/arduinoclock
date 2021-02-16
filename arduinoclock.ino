d5ef9d2dbb1051f2617f2cf850544b85e7005180
Pablo Gonzalez <pglez82@gmail.com>
2021-02-16 20:14:40 +0100

    saving power

--- arduinoclock.ino
+++ arduinoclock.ino
@@ -1,4 +1,9 @@
 #include <LiquidCrystal.h>
+#include <DHT.h>
+#include "LowPower.h"
+
+#define DHTTYPE DHT22
+
 //LCD pin to Arduino
 const int pin_RS = 8; 
 const int pin_EN = 9; 
@@ -7,7 +12,10 @@ const int pin_d5 = 5;
 const int pin_d6 = 6; 
 const int pin_d7 = 7; 
 const int pin_BL = 10; 
+const int DHTPin = 2;
+
 LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+DHT dht(DHTPin, DHTTYPE);
 
 int horas;
 int minutos;
@@ -21,6 +29,8 @@ boolean edicionHoras; //True si estamos editando la hora y false si no lo estamo
 boolean edicionMinutos;
 boolean parpadeoHoras;
 boolean parpadeoMinutos;
+
+const int bl_intensity = 1;
 
 
 void actualizarHora(){
@@ -55,7 +65,7 @@ void actualizarHora(){
 }
 
 void imprimirHora(){
-    lcd.setCursor(4,1);
+  lcd.setCursor(0,1);
   
   if (edicionHoras==true && parpadeoHoras==true)
   {
@@ -133,11 +143,37 @@ void gestionBotones(){
     edicionHoras = true;
 }
 
+void imprimirTempHum(){
+  if ((segundos==0 || segundos==20 || segundos==40) && decimas==2)
+  {
+     float t = dht.readTemperature();
+     Serial.println(t);
+     lcd.setCursor(11,1);
+     lcd.print(t,1);
+     lcd.print("C");
+  }
+  if ((segundos==10 || segundos==30 || segundos==50) && decimas==2)
+  {
+     float h = dht.readHumidity();
+     Serial.println(h);
+     lcd.setCursor(11,1);
+     if (h<100)
+       lcd.print("  ");
+     else
+       lcd.print(" ");
+     lcd.print(h,0);
+     lcd.print("%");
+    
+  }
+  
+}
+
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Comienzo...");
   pinMode(pin_BL, OUTPUT);
-  analogWrite(pin_BL, 10);
+  analogWrite(pin_BL, bl_intensity);
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.write(byte(0));
@@ -163,6 +199,7 @@ void setup() {
   parpadeoMinutos=false;
   luzContador=0;
   lcd.createChar(0, Heart);
+  dht.begin();
 }
 
 void loop() {  
@@ -171,16 +208,21 @@ void loop() {
   gestionBotones();
   actualizarHora();
   imprimirHora();
+  imprimirTempHum();
+
   
   if (luzContador>0)
     luzContador = luzContador - 1;
   
   if (luzContador==0)
   {
-    analogWrite(pin_BL, 10);
+    analogWrite(pin_BL, bl_intensity);
     luzContador = luzContador - 1;
   }
 
-  delay(193);
+  
+  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, 
+                SPI_OFF, USART0_OFF, TWI_OFF);
+  delay(75);
+  
 } 
-
